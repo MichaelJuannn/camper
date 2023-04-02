8089137f297d13fc4db8ca9f8fb530342c54ce17
MichaelJuannn <juan600d@gmail.com>
2023-04-02 21:04:50 +0700

    auth discord

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,18 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import DiscordProvider from 'next-auth/providers/discord';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/utils/prisma';
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
 	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	session: { strategy: 'jwt' },
 	providers: [
+		DiscordProvider({
+			clientId: process.env.DISCORD_CLIENT_ID!, // its a string check env
+			clientSecret: process.env.DISCORD_CLIENT_SECRET!, // its a string check env
+		}),
 		CredentialsProvider({
 			name: 'Credentials',
 			credentials: {
@@ -12,10 +21,11 @@ export const authOptions = {
 			},
 			async authorize(credentials) {
 				const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
-				if (user) {
+				console.log(credentials);
+				if (credentials?.username == 'dragon') {
 					return user;
 				} else {
-					return null;
+					throw new Error('check your mom');
 				}
 			},
 		}),
